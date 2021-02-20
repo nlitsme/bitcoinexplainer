394ba92939ce27390b4c3b20148bbf9079b436d5
itsme <itsme@xs4all.nl>
2021-02-21 00:00:53 +0100

    ecdsa: adding scalar conversion where needed.

--- ecdsa.js
+++ ecdsa.js
@@ -1,43 +1,81 @@
+/* Elliptic curve Digital Signature Algorithm */
+
 class ECDSA {
     constructor(ec, g)
     {
         this.ec = ec;
         this.g = g;
     }
-    scalar(x)
-    {
-        return this.ec.order.value(x);
-    }
+    // changes 'x' to be of galois field 'n'
+    scalar(x) { return this.ec.order.value(x); }
 
+    /* calculate a public key from a private key.
+     *
+     * Return value: the public key point.
+     */
     calcpub(x)
     {
         return this.g.mul(x);
     }
+
+    /* create a signature.
+     *
+     * Return value: a tuple of 'r' and 's'.
+     */
     sign(m, x, k)
     {
         var R = this.g.mul(k);
-        var s = m.add(x.mul(R.x)).div(k);
+        var s = m.add(x.mul(this.scalar(R.x))).div(k);
         return [R.x, s];
     }
+
+    /* Verify a signature.
+     *
+     * Return value: true when the signature is correct.
+     */
     verify(m, Y, r, s)
     {
         var R = this.g.mul(m).add(Y.mul(r)).div(s);
-        return r==R.x;
+        return r.equals(this.scalar(R.x));
     }
+
+    /* Crack a key given a pair of signatures with the same 'r'.
+     *
+     * Return value: a tuple of the signing secret, and the secret key.
+     */
     crack2(r, m1, m2, s1, s2)
     {
         var k = m1.sub(m2).div(s1.sub(s2))
-        var x1 = this.crack1(k, m1, r, s1);
-        var x2 = this.crack1(k, m2, r, s2);
+        var x1 = this.crack1r(k, m1, r, s1);
+        var x2 = this.crack1r(k, m2, r, s2);
         if (x1.equals(x2))
             return [k, x1];
+        console.log("unexpectedly: x1 != x2");
         console.log("k=", k);
         console.log("x1=", x1);
         console.log("x2=", x2);
         return [0, 0];
     }
-    crack1(k, m, r, s)
+
+    /* this variant of crack1 assumes we already have calculated 'r',
+     * and can directly calculate 'x'.
+     *
+     * Return value: the secret key
+     */
+    crack1r(k, m, r, s)
     {
         return s.mul(k).sub(m).div(r);
+    }
+
+    /* this variant of crack1, calculates r, then calls crack1r.
+     *
+     * Return value: the secret key
+     */
+    crack1(k, m, s)
+    {
+        var R = this.g.mul(k);
+        // we have to convert the x coord of R from the galois field with order 'p'
+        // to the field with order 'n'
+        return this.crack1r(k, m, this.scalar(R.x), s);
     }
 };
